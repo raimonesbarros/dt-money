03b986a0adeb907071909a63f95bf5b1f800e385
Raimones Barros <raimonesrg3@gmail.com>
2023-06-03 13:33:21 -0300

    feat: create SearchForm component

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,5 +1,6 @@
 import { Header } from "../../components/Header";
 import { Summary } from "../../components/Summary";
+import { SearchForm } from "./components";
 import {
   PriceHighLight,
   TransactionsContainer,
@@ -13,6 +14,9 @@ export function Transactions() {
       <Summary />
 
       <TransactionsContainer>
+
+        <SearchForm />
+
         <TransactionsTable>
           <tbody>
             <tr>
